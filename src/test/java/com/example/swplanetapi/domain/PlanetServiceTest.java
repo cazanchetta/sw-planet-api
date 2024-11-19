843a27cfd946f6fd8becdf87eb6b6ac8f5ef128b
Carlos Zanchetta <cazanchetta@gmail.com>
2024-11-19 08:04:01 -0300

    Teste de unidade

--- src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -1,24 +1,52 @@
 package com.example.swplanetapi.domain;
 
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
 
-    @Autowired
+    @InjectMocks
+//    @Autowired
     private PlanetService planetService;
+
+    @Mock
+//    @MockBean
+    private PlanetRepository planetRepository;
 
     @Test
     // operacao_estado_retorno
     public void createPlanet_WithValidData_ReturnsPlanet() {
+        // AAA
+
+        // Arrange
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        // Act
         // system under test
         Planet sut = planetService.create(PLANET);
 
+        // Assert
         assertThat(sut).isEqualTo(PLANET);
     }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
 }
